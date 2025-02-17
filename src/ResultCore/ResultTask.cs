670e717d68f4c5c1fd208f3eb43af877e5be4225
jerviscui <cuijervis@126.com>
2025-02-17 18:56:15 +0800

    Add AsyncResultTaskMethodBuilder

--- src/ResultCore/ResultTask.cs
+++ src/ResultCore/ResultTask.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -16,7 +17,7 @@ namespace ResultCore;
     "Async/await",
     "CRR0035:No CancellationToken parameter in the asynchronous method",
     Justification = "<Pending>")]
-//[AsyncMethodBuilder(typeof(AsyncValueTaskMethodBuilder<>))]
+[AsyncMethodBuilder(typeof(AsyncResultTaskMethodBuilder<>))]
 [StructLayout(LayoutKind.Auto)]
 public readonly record struct ResultTask<TError>
     where TError : BasicError, new()
@@ -35,6 +36,11 @@ public readonly record struct ResultTask<TError>
     public static implicit operator ResultTask<TError>(Result<TError> result) => new(result);
 
     #endregion
+
+    internal ResultTask(ValueTask<Result<TError>> task)
+    {
+        _task = task;
+    }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="Result{TError}"/> with default <typeparamref name="TError"/>.
@@ -101,6 +107,88 @@ public readonly record struct ResultTask<TError>
     public ValueTask<Result<TError>> Preserve()
     {
         return _task.Preserve();
+    }
+
+    #endregion
+
+}
+
+[SuppressMessage("Members", "CRR0026:Unused member", Justification = "<Pending>")]
+public class AsyncResultTaskMethodBuilder<TResult>
+    where TResult : BasicError, new()
+{
+
+    #region Constants & Statics
+
+    // 1. 创建一个 AsyncTaskMethodBuilder
+#pragma warning disable CA1000 // Do not declare static members on generic types
+    public static AsyncResultTaskMethodBuilder<TResult> Create()
+#pragma warning restore CA1000 // Do not declare static members on generic types
+    {
+        var builder = AsyncValueTaskMethodBuilder<Result<TResult>>.Create();
+
+        return new AsyncResultTaskMethodBuilder<TResult>(builder);
+    }
+
+    #endregion
+
+    private readonly AsyncValueTaskMethodBuilder<Result<TResult>> _builder;
+
+    public AsyncResultTaskMethodBuilder(AsyncValueTaskMethodBuilder<Result<TResult>> builder)
+    {
+        _builder = builder;
+    }
+
+    #region Properties
+
+    // 5. 作为 async 方法的返回值
+    public ResultTask<TResult> Task => new(_builder.Task);
+
+    #endregion
+
+    #region Methods
+
+    // 3. 将状态机的 MoveNext 方法注册为 async 方法内 await 的 Task 的回调
+    public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+        where TAwaiter : INotifyCompletion
+        where TStateMachine : IAsyncStateMachine
+    {
+        _builder.AwaitOnCompleted(ref awaiter, ref stateMachine);
+    }
+
+    // 4. 同 AwaitOnCompleted，但是清空 ExecutionContext
+    public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+        where TAwaiter : ICriticalNotifyCompletion
+        where TStateMachine : IAsyncStateMachine
+    {
+        _builder.AwaitUnsafeOnCompleted(ref awaiter, ref stateMachine);
+    }
+
+    // 7. SetException
+    public void SetException(Exception exception)
+    {
+        _builder.SetException(exception);
+    }
+
+    // 8. SetResult
+    public void SetResult(TResult result)
+    {
+        _builder.SetResult(result);
+    }
+
+    // 6. 绑定状态机，但编译器的编译结果不会调用
+#pragma warning disable IDE0060 // Remove unused parameter
+    public void SetStateMachine(IAsyncStateMachine stateMachine)
+#pragma warning restore IDE0060 // Remove unused parameter
+    {
+        _builder.SetStateMachine(stateMachine);
+    }
+
+    // 2. 开始执行 AsyncTaskMethodBuilder 及其绑定的状态机
+    public void Start<TStateMachine>(ref TStateMachine stateMachine)
+        where TStateMachine : IAsyncStateMachine
+    {
+        _builder.Start(ref stateMachine);
     }
 
     #endregion
